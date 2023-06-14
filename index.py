cad1c5ba98d54878a954590234c2c1a5f340a52c
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2023-06-14 14:36:18 -0400

    Sup Biel

---
+++ index.py
@@ -0,0 +1,16 @@
+# []
+# {}
+# ()
+
+mylist = ["apple", "banana", "cherry", 1, 2, 3]
+print(mylist)
+
+# print(len(mylist))
+
+mylist.append("orange")
+
+for caro in mylist:
+    print(caro)
+# apple
+# banana
+# cherry
